5bc0a3a03e6ff9146d5bb711f7ce365553cb1c02
Angelo Manos <angelo@angelomanos.com>
2017-09-11 23:05:28 +0000

    cleaning up

--- App.js
+++ App.js
@@ -1,83 +1,81 @@
 import React, { Component } from 'react';
-import ClockFace from './res/ClockElements';
+import ClockFace from './Components/ClockElements';
 
 export default class Clock extends Component {
-  constructor(props) {
+  constructor( props ) {
     super(props);
-    this.state = { time : 'Setting time...', secondsCount:1};
+    this.state = { time : 'Setting time...', secondsCount : 1 };
 
     this.updateTime = this.updateTime.bind(this);
   }
 
-  updateTime(time, seconds, minutes, hours) {
+  updateTime( time, seconds, minutes, hours ) {
     let extra = 0;
-    if(seconds === 0){
-      this.setState({secondsCount: this.state.secondsCount + 1})
+
+    if ( seconds === 0 ) {
+      this.setState({ secondsCount : this.state.secondsCount + 1 })
     }
-    if(this.state.secondsCount > 0 ){
+
+    if ( this.state.secondsCount > 0 ) {
       extra = 360 * this.state.secondsCount;
     }
+
     let secondsRotation = (seconds * 6) + extra;
-
-    //secondsRotation = 360 * this.state.secondsCount;
-
-    let minuteRotation = minutes * 6;
-    let hourRotation = hours * 30;
-
-    console.log(secondsRotation, seconds);
-
+    let minuteRotation  = minutes * 6;
+    let hourRotation    = hours * 30;
 
     this.setState({
-      time,
-      secondsRotation,
-      minuteRotation,
-      hourRotation
-    });
+                    time,
+                    secondsRotation,
+                    minuteRotation,
+                    hourRotation
+                  });
   }
 
   componentDidMount() {
     let _this = this;
-    setInterval(function(){
+    setInterval(function () {
       let currentDate = new Date();
-      let display = currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
-      let seconds = currentDate.getSeconds();
-      let minutes = currentDate.getMinutes() + (seconds / 60);
-      let hours = currentDate.getHours() + (minutes / 60);
+      let display     = currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
+      let seconds     = currentDate.getSeconds();
+      let minutes     = currentDate.getMinutes() + (seconds / 60);
+      let hours       = currentDate.getHours() + (minutes / 60);
 
       _this.updateTime(display, seconds, minutes, hours);
     }, 1000)
   }
+
   render() {
     let styles = {
       container : {
-        width: '100%',
-        height: '100%',
-        position:'relative',
+        width : '100%',
+        height : '100%',
+        position : 'relative',
       },
       secondHand : {
-        transformOrigin: 'bottom',
-        transform: 'rotate('+this.state.secondsRotation+'deg)',
-        background:'black',
-        width:'1px',
-        height:'45%',
-        position:'absolute',
-        left:'50%',
-        bottom:'50%',
-        transition: 'transform 300ms ease-in'
-  },
+        transformOrigin : 'bottom',
+        transform : 'rotate(' + this.state.secondsRotation + 'deg)',
+        background : 'black',
+        width : '1px',
+        height : '45%',
+        position : 'absolute',
+        left : '50%',
+        bottom : '50%',
+        transition : 'transform 300ms ease-in'
+      },
       minuteHand : {
-        transformOrigin: 'bottom',
-        position:'absolute',
-        left:'50%',
-        bottom:'50%',
-        transition: 'transform 300ms ease-in'
+        transformOrigin : 'bottom',
+        position : 'absolute',
+        left : '50%',
+        bottom : '50%',
+        transition : 'transform 300ms ease-in'
       },
       hourHand : {
-        transformOrigin: 'bottom',
-        position:'absolute',
-        left:'50%',
-        bottom:'50%',
-        transition: 'transform 300ms ease-in'
+        transformOrigin : 'bottom',
+        position : 'absolute',
+        left : '50%',
+        bottom : '50%',
+        transition : 'transform 300ms ease-in'
       }
     };
 
@@ -90,7 +88,7 @@ export default class Clock extends Component {
           minuteStyle={styles.minuteHand}
           minuteRotation={this.state.minuteRotation}
           hourRotation={this.state.hourRotation}
-          style={{width:'100%', height:'100%'}} />
+          style={{ width : '100%', height : '100%' }}/>
         {/*{ this.state.time }*/}
       </div>
     );
