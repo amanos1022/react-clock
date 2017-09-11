9d40fe06decc0015d242f4eecc3f239ed229e3e1
Angelo Manos <angelo@angelomanos.com>
2017-09-11 16:29:12 +0000

    adding actual graphics, wrestling with responsive svgs

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import {HourHand, MinuteHand, MiddleCircle} from './res/HourHand';
 
 export default class Clock extends Component {
   constructor(props) {
     super(props);
-    this.state = { time : 'Setting time...', rotation:0};
+    this.state = { time : 'Setting time...', secondsCount:1};
 
     this.updateTime = this.updateTime.bind(this);
   }
 
-  updateTime(time, seconds) {
-    let rotation = seconds * 6;
+  updateTime(time, seconds, minutes, hours) {
+    let extra = 0;
+    if(seconds === 0){
+      this.setState({secondsCount: this.state.secondsCount + 1})
+    }
+    if(this.state.secondsCount > 0 ){
+      extra = 360 * this.state.secondsCount;
+    }
+    let secondsRotation = (seconds * 6) + extra;
+
+    //secondsRotation = 360 * this.state.secondsCount;
+
+    let minuteRotation = minutes * 6;
+    let hourRotation = hours * 30;
+
+    console.log(secondsRotation, seconds);
+
+
     this.setState({
       time,
-      rotation
+      secondsRotation,
+      minuteRotation,
+      hourRotation
     });
   }
 
@@ -22,27 +41,72 @@ export default class Clock extends Component {
       let currentDate = new Date();
       let display = currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
       let seconds = currentDate.getSeconds();
+      let minutes = currentDate.getMinutes();
+      let hours = currentDate.getHours();
 
-      _this.updateTime(display, seconds);
+      _this.updateTime(display, seconds, minutes, hours);
     }, 1000)
   }
   render() {
     let styles = {
-      default : {
-        width: '33%',
-        margin:'40% auto',
-        color: 'white',
-        background:'#111',
+      container : {
+        width: '400px',
+        height: '400px',
+        margin:'10% auto',
         textAlign:'center',
         fontSize: '3em',
-        transform: 'rotate('+this.state.rotation+'deg)'
-
+        border:'1px solid #555',
+        position:'relative',
+      },
+      secondHand : {
+        transformOrigin: 'bottom',
+        transform: 'rotate('+this.state.secondsRotation+'deg)',
+        background:'black',
+        width:'1px',
+        height:'45%',
+        position:'absolute',
+        left:'53%',
+        bottom:'50%',
+        transition: 'transform 300ms ease-in'
+  },
+      minuteHand : {
+        transformOrigin: 'bottom',
+        transform: 'rotate('+this.state.minuteRotation+'deg)',
+        width:'6%',
+        height:'45%',
+        position:'absolute',
+        left:'50%',
+        bottom:'50%',
+        transition: 'transform 300ms ease-in'
+      },
+      hourHand : {
+        transformOrigin: 'bottom',
+        transform: 'rotate('+this.state.hourRotation+'deg)',
+        width:'7%',
+        height:'33%',
+        position:'absolute',
+        left:'50%',
+        bottom:'50%',
+        transition: 'transform 300ms ease-in'
+      },
+      circle : {
+        width:'15%',
+        height: '15%',
+        position: 'absolute',
+        left: '44%',
+        top: '39%',
       }
+
     };
 
     return (
-      <div style={styles.default}>
-        { this.state.time }
+      <div style={styles.container}>
+        <div style={styles.secondHand}>
+        </div>
+        <MinuteHand style={styles.minuteHand} />
+        <HourHand style={styles.hourHand} />
+        <MiddleCircle style={styles.circle} />
+        {/*{ this.state.time }*/}
       </div>
     );
   }
